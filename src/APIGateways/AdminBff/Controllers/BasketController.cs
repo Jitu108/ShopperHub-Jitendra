0f2c5304b2093e0e2ab7b65840342cdabaa73c97
Jitendra Kumar Gupta <52508596+Jitu108@users.noreply.github.com>
2023-09-15 03:14:16 +0530

    Partial work on ShoppingCart Bff.

---
+++ src/APIGateways/AdminBff/Controllers/BasketController.cs
@@ -0,0 +1,40 @@
+﻿using AdminBff.Dtos;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdminBff.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        [HttpGet("{userId}", Name = "GetBasket")]
+        [ProducesResponseType(typeof(ShoppingCartDto), StatusCodes.Status200OK)]
+        public async Task<ActionResult<ShoppingCartDto>> GetBasket(string userId)
+        {
+            //var basket = await repo.GetBasket(userId);
+            //return Ok(basket ?? new ShoppingCart(userId));
+            return Ok();
+        }
+
+        [HttpPost("{userId}", Name = "UpdateBasket")]
+        [ProducesResponseType(typeof(ShoppingCartDto), StatusCodes.Status200OK)]
+        public async Task<ActionResult<ShoppingCartDto>> UpdateBasket(string userId, [FromBody] ShoppingCartItemCreate item)
+        {
+            //var itemModel = mapper.Map<ShoppingCartItemDto>(item);
+
+            //return Ok(await repo.UpdateBasket(userId, itemModel));
+
+            return Ok();
+        }
+
+        [HttpDelete("{userId}", Name = "DeleteBasket")]
+        [ProducesResponseType(typeof(void), StatusCodes.Status200OK)]
+        public async Task<IActionResult> DeleteBasket(string userId)
+        {
+            //await repo.DeleteBasket(userId);
+            return Ok();
+        }
+    }
+}
---
+++ src/APIGateways/AdminBff/Dtos/ShoppingCartDto.cs
@@ -0,0 +1,12 @@
+﻿
+namespace AdminBff.Dtos
+{
+    public class ShoppingCartDto
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public List<ShoppingCartItemDto> Items { get; set; }
+
+        public decimal TotalPrice { get; set; }
+    }
+}
